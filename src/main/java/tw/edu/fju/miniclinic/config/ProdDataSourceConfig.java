ed30ff2d7b2cb258c2b9c8b9c2eed9c0c1e78b71
414571037 <414571037@m365.fju.edu.tw>
2026-06-07 04:08:25 +0800

    [NO-AI] 新增正確資料庫主機名.

--- src/main/java/tw/edu/fju/miniclinic/config/ProdDataSourceConfig.java
+++ src/main/java/tw/edu/fju/miniclinic/config/ProdDataSourceConfig.java
@@ -2,4 +2,10 @@ package tw.edu.fju.miniclinic.config;
 
 public class ProdDataSourceConfig {
     public static final String DATABASE_URL = "jdbc:postgresql://dpg-d8e7n14p3tds738agof0-a:5432/miniclinic_db_ce35";
+    public static final String DATABASE_USERNAME = "miniclinic_db_ce35_user";
+    public static final String DATABASE_PASSWORD = "cscCfvT6QBGO7B7hNzAEt3ZFHlmyirqw";  
+    public static final String SPRING_DATASOURCE_URL = "jdbc:postgresql://dpg-d8e7n14p3tds738agof0-a:5432/miniclinic_db_ce35";
+    public static final String SPRING_DATASOURCE_USERNAME = "miniclinic_db_ce35_user";
+    public static final String SPRING_DATASOURCE_PASSWORD = "cscCfvT6QBGO7B7hNzAEt3ZFHlmyirqw";
+    // postgresql://miniclinic_db_ce35_user:cscCfvT6QBGO7B7hNzAEt3ZFHlmyirqw@dpg-d8e7n14p3tds738agof0-a/miniclinic_db_ce35
 }
